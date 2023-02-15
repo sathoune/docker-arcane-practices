aabeaf08be7aaa7d14878e8ba012a012446d79e5
sathoune <michal5bargiel@gmail.com>
2023-02-15 02:29:40 +0100

    🔥 Yeet out `phoenix` app.

--- dind/ci-scripts/push-images.sh
+++ dind/ci-scripts/push-images.sh
@@ -4,9 +4,5 @@ docker compose up -d --build
 docker compose down
 
 docker tag app-python-app "$DOCKER_REGISTRY_URL"/"$PYTHON_IMAGE"
-docker tag app-elixir-app "$DOCKER_REGISTRY_URL"/"$ELIXIR_IMAGE"
-docker tag app-postgres "$DOCKER_REGISTRY_URL"/"$POSTGRES_IMAGE"
 
 docker push "$DOCKER_REGISTRY_URL"/"$PYTHON_IMAGE"
-docker push "$DOCKER_REGISTRY_URL"/"$ELIXIR_IMAGE"
-docker push "$DOCKER_REGISTRY_URL"/"$POSTGRES_IMAGE"

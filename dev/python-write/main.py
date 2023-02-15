54f434681366410eb46d41c947af2ac529fe9568
sathoune <michal5bargiel@gmail.com>
2023-02-15 03:37:54 +0100

    🐛 Add CORS.

--- dev/python-write/main.py
+++ dev/python-write/main.py
@@ -1,10 +1,16 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
 from session import initialize_redis
 
 app = fastapi.FastAPI()
 
-
+c = CORSMiddleware(
+    app,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.on_event("startup")
 async def startup_event():
     app.state.redis = await initialize_redis()

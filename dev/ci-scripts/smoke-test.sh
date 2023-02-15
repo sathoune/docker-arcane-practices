1d9bba4dd74607a1036c09f8e3f9d56443bd5915
sathoune <michal5bargiel@gmail.com>
2023-02-15 05:15:23 +0100

    ♻️ Parametrize smoke test.

--- dev/ci-scripts/smoke-test.sh
+++ dev/ci-scripts/smoke-test.sh
@@ -4,11 +4,11 @@ docker compose -f compose.yml -f compose.ci.yml up -d --wait
 sleep 5
 
 printf "\n\nRead server test \n"
-curl --fail localhost:8000/healtz
+curl --fail "${SMOKE__READ_BACKEND_HOST}"/healtz
 
 printf "\n\nWrite server test\n"
-curl --fail localhost:8008/healtz
+curl --fail "${SMOKE__WRITE_BACKEND_HOST}"/healtz
 printf "\n\nClient test\n"
-curl --fail localhost:3000
+curl --fail "${SMOKE__FRONTEND_HOST}"
 
 docker compose down

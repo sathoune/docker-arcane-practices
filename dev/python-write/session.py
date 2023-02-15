f1a58e6fd18387ec2c319bdacd169c85c4400be0
sathoune <michal5bargiel@gmail.com>
2023-02-15 04:19:42 +0100

    ♻️ Parametrize redis address.

--- dev/python-write/session.py
+++ dev/python-write/session.py
@@ -3,12 +3,12 @@ import redis.asyncio as redis
 
 
 class RedisSettings(pydantic.BaseSettings):
-    redis_url: str = "redis://redis:6379/0"
+    redis_host: str
 
 
 async def initialize_redis() -> redis.Redis:
     return await redis.from_url(
-        RedisSettings().redis_url,
+        f"redis://{RedisSettings().redis_host}/0",
         encoding="utf-8",
         decode_responses=True,
     )

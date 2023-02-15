0177ce054d526b1e22a0a027a3696cb76f4176c1
sathoune <michal5bargiel@gmail.com>
2023-02-15 03:57:28 +0100

    💄 Beautify UI.

--- dev/frontend/src/App.tsx
+++ dev/frontend/src/App.tsx
@@ -7,7 +7,12 @@ function App() {
   const [message, setMessage] = useState("")
   const [storedMessage, setStoredMessage] = useState("")
 
-  const fetchCall = async () => {
+  const write = async () => {
+    await writeCall(message)
+    setMessage("")
+  }
+
+  const read = async () => {
     const data = await readCall()
     setStoredMessage(data)
   }
@@ -15,28 +20,22 @@ function App() {
   return (
     <div className="App">
       <h1>Some Docker thingy</h1>
-
-      <div>
-        <p>Store the input</p>
-        <div>
-          <input type="text" onChange={(e) => setMessage(e.target.value)}/>
+      <div
+        style={{display: "flex", flexDirection: "row", alignItems: "center", width: "100%",}}
+      >
+        <div style={{margin: "auto"}}>
+          <h2 style={{marginBottom: "2rem"}}>Store the message</h2>
+          <div style={{marginBottom: "2rem"}}>
+            <input type="text" onChange={(e) => setMessage(e.target.value)} value={message}/>
+          </div>
+          <button onClick={write}>Save</button>
         </div>
-        <button onClick={
-          () => writeCall(message)
-        }>
-          Save
-        </button>
 
-      </div>
-
-      <div>
-        <p>Read the message</p>
-        <button
-          onClick={fetchCall}
-        >
-          fetch
-        </button>
-        <p>Value: {storedMessage}</p>
+        <div style={{margin: "auto"}}>
+          <h2 style={{marginBottom: "2rem"}}>Read the message</h2>
+          <button style={{marginBottom: "2rem"}} onClick={read}>fetch</button>
+          <p>Value: {storedMessage}</p>
+        </div>
       </div>
 
     </div>
